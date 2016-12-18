a2f17463208d99076ea8d51f178b46eececab238
alexqin@intv.com.cn <alexqin@intv.com.cn>
2016-12-19 01:48:52 +0800

    init the repo

---
+++ dev/js/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import UserList from '../containers/user-list';
+import UserDetails from '../containers/user-detail';
+
+require('../../sass/style.sass');
+
+const App = () => (
+    <div>
+        <h2>User List</h2>
+        <UserList />
+        <hr />
+        <h2>User Details</h2>
+        <UserDetails />
+    </div>
+);
+
+export default App;
